41b2979dd2f7c68622e714436c4327013cd91ccf
Julian Lannigan <julian.lannigan@gmail.com>
2017-04-04 13:27:58 -0400

    adding no-param-reassign rule to exclude prop reassignment

--- node.js
+++ node.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: require.resolve('eslint-config-airbnb-base'),
     rules: {
       'comma-dangle': 'off',
-      indent: ['error', 4]
+      indent: ['error', 4],
+      'no-param-reassign': ['error', { 'props': false }]
     }
 };
